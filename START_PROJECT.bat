27a23431db51e88d9cd754ad8e6e1f0eabb373e6
MANASVI005 <manasvibanthiya@gmail.com>
2026-04-12 23:36:21 +0530

    Fix Vercel deployment: rename backend to api

--- START_PROJECT.bat
+++ START_PROJECT.bat
@@ -2,7 +2,7 @@
 echo Starting Green Bin Nexus...
 
 :: Start PHP Backend in a new window
-start "PHP Backend" cmd /k "C:\xampp\php\php.exe -S 127.0.0.1:8001 -t php-backend"
+start "PHP Backend" cmd /k "C:\xampp\php\php.exe -S 127.0.0.1:8001 -t api"
 
 :: Start Vite Frontend in a new window
 echo Starting Frontend...
